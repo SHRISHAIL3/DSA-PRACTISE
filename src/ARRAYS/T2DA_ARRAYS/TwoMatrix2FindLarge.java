4a7ee73cc4806bc4796f9a112385b9a04100c643
SHRISHAIL <shrishailbelle6@gmail.com>
2025-11-13 18:03:32 +0530

    ADDED NEW PACKAGE FOR 2D MATRIX 1)HOW TO READ 2D MATRIX 2)HOW TO TRAVEL 2d MATRIX 3)FINDD LARGES NUMBER IN 2d

---
+++ src/ARRAYS/T2DA_ARRAYS/TwoMatrix2FindLarge.java
@@ -0,0 +1,42 @@
+package ARRAYS.T2DA_ARRAYS;
+
+import java.util.Scanner;
+
+public class TwoMatrix2FindLarge {
+	
+	public static void  Large(int matrix[][]) {
+		int large=Integer.MIN_VALUE;
+		for (int i1 = 0; i1 < matrix.length; i1++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				large=Math.max(large, matrix[i1][j]);
+				
+			}		
+			}
+		System.out.println("LARGEST "+large);
+	
+		
+		
+	}
+
+public static void main(String[] args) {
+	int matrix[][]= new int[3][3];
+	Scanner scanner=new Scanner(System.in);
+	for (int i = 0; i < matrix.length; i++) {
+		for (int j = 0; j < matrix[0].length; j++) {
+			matrix[i][j]=scanner.nextInt();	
+		}
+	}
+		for (int i1 = 0; i1 < matrix.length; i1++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				System.out.print(matrix[i1][j]);
+			}
+			System.out.println();
+				
+			}
+		scanner.close();
+		Large(matrix);
+	}
+	
+	
+}
+
--- src/Random/WaterBottle1518.java
+++ src/Random/WaterBottle1518.java
@@ -1,5 +1,26 @@
 package Random;
 
 public class WaterBottle1518 {
+	
+	    public static int numWaterBottles(int numBottles, int numExchange) {
+	        int drink=numBottles;
+	        while(numBottles>=numExchange){
+	          int newnumBottles=numBottles/numExchange;
+	          int left=numBottles%numExchange;
+	          drink=drink+newnumBottles;
+	          numBottles = newnumBottles + left; 
 
-}
+	           
+	        }
+
+	        return drink;
+	        
+	    }
+	    public static void main(String[] args) {
+		
+			System.out.println(	numWaterBottles(25, 5));
+		}
+	}
+ 
+
+
